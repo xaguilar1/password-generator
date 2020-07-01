4c7a59c86261d9375b76289552f82b8a1be10e3c
xray <xyaguilar77@gmail.com>
2020-06-30 23:42:35 -0400

    changes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Password-Generator-
+
+Details: the meaning of this is to generate a password that contains a certain number of charachters & type of charachters generate as a password based on the users response
+
+
+- created an addEventListener to generate my button with my first variable prompt asking how many charachters the user would want the password to be 
+
+- created a while loop in order to provide a bases for how long the password length can be 
+
+- created confirm variables that the user can either confirm or cancel 
+
+- added a while loop to have the user at least choose one of the special charachters for password 
+
+- created a function to generate the new password on the page with for loops to go through each of my charachters of each variable (lowercase letters, uppercase letters, symbols, numbers)
+
+
+Overall: this one was difficult trying to remember how to connect and name functions to not scramble things. It doesnt seem like i was able to get the random password, though my logic seems to be in the right area. I had my 'if' functions allow the password to go through each of the charachters in my strings. I got stuck trying to figure out how to connect the if functions to scramble the charachters based on user choice to show up in the generator. it showed up : undefined. I will be working on this more in the future and maybe even add some more things to make it more interesting, but for now this is as far as i seem to have been able to go.
--- script.js
+++ script.js
@@ -1,54 +1,120 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = true;
+var lowerCase = true; 
+var numbers = true; 
+var symbols = true;
+var PassLength = 0;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generateNewPassword();
+  var password = generatePassword();
+  // generateNewPassword();
+  generateNewPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword(){
 
 
-var lengthOfPass = Number(prompt("How many characters would you like your password to be?"));
-while (isNaN(lengthOfPass) || lengthOfPass < 8 || lengthOfPass > 128) lengthOfPass = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+//after clicking button, this is the prompt that will pop up to the user 
+PassLength = prompt("How many characters would you like your password to be?");
 
-var symbols = confirm("Would you like to use special characters?");
-var numbers = confirm("Would you like to use numbers?");
-var lowercase = confirm("Would you like to use lowercase letters?");
-var uppercase = confirm("Would you like to use uppercase letters?");
+//while loop (repeating ifstatement if requirements not met) with condition and executed code 
+while ( PassLength < 8 || PassLength > 128 || PassLength === isNaN ) {
+PassLength = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
+}
 
+//user confirms or cancels these charachters 
+symbols = confirm("Would you like to use symbol characters?");
+numbers = confirm("Would you like to use numbers?");
+lowerCase = confirm("Would you like to use lowercase letters?");
+upperCase = confirm("Would you like to use uppercase letters?");
 
-while (!uppercase && !lowercase && !numbers && !symbols) {
+//this pops up if the user doesnt choose any charachters at all, must choose at least one 
+//! means not equal to, meaning IF user choosing cancel on all of them, the following alert will occur 
+while (!upperCase && !lowerCase && !numbers && !symbols) {
   alert("You must select at least one character type!");
-  symbols = confirm("Would you like to use special characters?");
+  symbols = confirm("Would you like to use s characters?");
   numbers = confirm("Would you like to use numbers?");
-  lowercase = confirm("Would you like to use lowercase letters?");
-  uppercase = confirm("Would you like to use uppercase letters?");
+  lowerCase = confirm("Would you like to use lowercase letters?");
+  upperCase = confirm("Would you like to use uppercase letters?");
   
 }
 
-window.addEventListener('load', function() {
-  generateNewPassword();
-});
+}
 
+////////////////////////////////////////////////////////////////// 2nd function below to generate new password 
+
+// #1: When I click the butotn, I get a prompt asking generatePassword()
+//  #2 Then I STORE the values from that prompt, and based on that, make a password generateNewPassword()
+
+//function to generate new password with different chosen charachters from user 
+// This function is only DEFINED and not running
+// YOu will need to call it somewhere. 
 function generateNewPassword() {
+  var upperCaseLetters = "QWERTYUIOPASDFGHJKLZXCVBNM"; 
+  var lowerCaseLetters = "qwertyuiopasdfghjklzxcvbnm";
+  var numberChar = "1234567890";
+  var symbolChar = "!@#$%^&*(){}[]=<>/,.";
   var password = "";
+  
+  
+  for (var i = 0; i < PassLength; i++) {
+    var upperCasePosition = Math.floor(Math.random()* upperCaseLetters.length) + 1;
+    console.log(upperCasePosition);
 
-  var allowed = {};
-  if (uppercase) password += random(allowed.uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM");
-  if (lowercase) password += random(allowed.lowercase = "qwertyuiopasdfghjklzxcvbnm");
-  if (numbers) password += random(allowed.numbers = "1234567890");
-  if (symbols) password += random(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
+    if(upperCase) {
+      splitUpperChar = upperCaseLetters.split('');
+      console.log(splitUpperChar)
+      password += splitUpperChar[upperCasePosition]
+    }
+  }
+  
+  for (var i = 0; i < PassLength; i++) {
+    var lowerCasePosition = Math.floor(Math.random()* lowerCaseLetters.length) + 1;
+    console.log(lowerCasePosition);
+  if(lowerCase) {
+    splitLowerChar = lowerCaseLetters.split('');
+    console.log(splitLowerChar)
+    password += splitLowerChar[lowerCasePosition]
+  }
+}
 
-  for (var i = password.length; i < lengthOfPass; i++) password += random(random(allowed).value);
+  for (var i = 0; i < PassLength; i++) {
+    var numberCharPosition = Math.floor(Math.random()* numberChar.length) + 1;
+    console.log(numberCharPosition);
+  if(numbers) {
+    splitNumberChar = numberChar.split('');
+    console.log(splitNumberChar)
+    password += splitNumberChar[numberCharPosition];
+  }
+}
 
-  document.getElementById("password").value = randomSequence(password).join("");
+  for (var i = 0; i < PassLength; i++) {
+    var symbolCharPosition = Math.floor(Math.random()* symbolChar.length) + 1;
+    console.log(symbolCharPosition);
+  if(symbols) {
+    splitSymbolChar = upperCaseLetters.split('');
+    console.log(splitSymbolChar)
+    password += splitSymbolChar[symbolCharPosition];
+  }
+}
+
+
+
+  var passwordText = document.getElementById("password") 
+  passwordText.textContent(password);
+  
+return password;
 
 }
+
 
